59123a63aac0f6af9f147f0f4aa0dfa60208c2ca
Neo404 <tell-tell-doll.a.fine.weather@docomo.ne.jp>
2019-12-03 23:25:30 +0900

    Unityエディタ拡張機能テスト

---
+++ Assets/Editor/CustomMenu.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class CustomMenu : MonoBehaviour {
+
+	// Unityエディタ拡張作成テスト
+	// shift + A で専用のショートカット
+	[MenuItem("CustomTools/ConsoleTest #a")]
+	public static void MenuConsole () {
+		Debug.Log("動作確認用 ログ");
+	}
+
+	// コンポーネントの歯車にメニューを追加します
+	[MenuItem("CONTEXT/Rigidbody/ShowLog")]
+	private static void ShowLog(MenuCommand menucommand) {
+		// 実行したRigidbodyを取得
+		Rigidbody rigidbody = menucommand.context as Rigidbody;
+
+		Debug.Log(rigidbody.gameObject.name);
+	}
+	
+}
