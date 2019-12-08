90b7966ee6a89c1153c979444d4e4fecf6ab5121
Neo404 <tell-tell-doll.a.fine.weather@docomo.ne.jp>
2019-12-08 14:54:24 +0900

    add フォルダのファイル数を表示するスクリプトテスト

---
+++ Assets/Editor/CustomFolderAssetCount.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
+
+public static class CustomFolderAssetCount
+{
+
+	[InitializeOnLoadMethod]
+	private static void ShowCount()
+	{
+		EditorApplication.projectWindowItemOnGUI += OnGUI;
+	}
+
+	private static void OnGUI(string guid, Rect selectionRect)
+	{
+		var dataPath = Application.dataPath;
+		var index = dataPath.LastIndexOf("Assets");
+		var dir = dataPath.Remove(index, "Assets".Length);
+		var path = dir + AssetDatabase.GUIDToAssetPath(guid);
+
+		var fileCount = Directory.GetFiles(path)
+				.Where( c => !c.EndsWith(".meta") )
+				.Count();
+
+		var dirCount = Directory.GetDirectories(path).Length;
+
+		var folderCount = fileCount + dirCount;
+
+		var showText = folderCount.ToString();
+		var showPos = selectionRect;
+
+		var label = EditorStyles.label;
+		var content = new GUIContent(showText);
+		var width = label.CalcSize(content).x;
+
+		showPos.x = showPos.xMax - width;
+		showPos.width = width;
+		showPos.yMin++;
+
+		GUI.Label(showPos, showText);
+	}
+}
