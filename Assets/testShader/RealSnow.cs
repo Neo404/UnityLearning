cca576897c0f0041eadf175dd11b0fccd55bf9df
Neo404 <tell-tell-doll.a.fine.weather@docomo.ne.jp>
2019-12-14 17:35:39 +0900

    add リアル風雪シェーダー作成

---
+++ Assets/testShader/RealSnow.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RealSnow : MonoBehaviour
+{
+	const int SNOW_NUM = 4000;
+	private Vector3[] vertices_;
+	private int[] triangles_;
+	private Color[] colors_;
+	private Vector2[] uvs_;
+
+	// 雪の範囲
+	private float range_;
+
+	// 雪範囲の逆数
+	private float rangeR_;
+
+	// 雪の移動量
+	private Vector3 move_ = Vector3.zero;
+
+	void Start()
+	{
+		range_ = 16.0f;
+		rangeR_ = 1.0f / range_;
+		vertices_ = new Vector3[SNOW_NUM*4];
+		for (var i = 0; i < SNOW_NUM; i++) {
+			float x = Random.Range (-range_, range_);
+			float y = Random.Range (-range_, range_);
+			float z = Random.Range (-range_, range_);
+			var point = new Vector3(x, y, z);
+			vertices_ [i*4+0] = point;
+			vertices_ [i*4+1] = point;
+			vertices_ [i*4+2] = point;
+			vertices_ [i*4+3] = point;
+		}
+
+		triangles_ = new int[SNOW_NUM * 6];
+		for (var i = 0; i < SNOW_NUM; i++) {
+			triangles_[i*6] = i*4;
+			triangles_[i * 6 + 1] = i * 4 + 1;
+			triangles_[i * 6 + 2] = i * 4 + 2;
+			triangles_[i * 6 + 3] = i * 4 + 2;
+			triangles_[i * 6 + 4] = i * 4 + 1;
+			triangles_[i * 6 + 5] = i * 4 + 3;
+		}
+
+		uvs_ = new Vector2[SNOW_NUM * 4];
+		for (var i = 0; i < SNOW_NUM; i++) {
+			uvs_ [i * 4 + 0] = new Vector2 (0f, 0f);
+			uvs_ [i * 4 + 1] = new Vector2 (1f, 0f);
+			uvs_ [i * 4 + 2] = new Vector2 (0f, 1f);
+			uvs_ [i * 4 + 3] = new Vector2 (1f, 1f);
+		}
+
+		Mesh mesh = new Mesh();
+		mesh.name = "SnowMesh";
+		mesh.vertices = vertices_;
+		mesh.triangles = triangles_;
+		mesh.colors = colors_;
+		mesh.uv = uvs_;
+		mesh.bounds = new Bounds(Vector3.zero, Vector3.one * 100000);
+		var meshFilter = GetComponent<MeshFilter>();
+		meshFilter.sharedMesh = mesh;
+	}
+
+	void LateUpdate()
+	{
+		var target_position = Camera.main.transform.TransformPoint(Vector3.forward * range_);
+		var meshRenderer = GetComponent<Renderer>();
+		meshRenderer.material.SetFloat("_Range", range_);
+		meshRenderer.material.SetFloat("_RangeR", rangeR_);
+		meshRenderer.material.SetFloat("_Size", 0.1f);
+		meshRenderer.material.SetVector("_MoveTotal", move_);
+		meshRenderer.material.SetVector("_CamUp", Camera.main.transform.up);
+		meshRenderer.material.SetVector("_TargePosition", target_position);
+
+		float x = (Mathf.PerlinNoise(0f, Time.time * 0.1f) - 0.5f) * 10.0f; 
+		float y = -2.0f;
+		float z = (Mathf.PerlinNoise(0f, Time.time * 0.1f) - 0.5f) * 10.0f; 
+
+		move_ += new Vector3(x, y, z) * Time.deltaTime;
+		move_.x = Mathf.Repeat(move_.x, range_ * 2.0f);
+		move_.y = Mathf.Repeat(move_.y, range_ * 2.0f);
+		move_.z = Mathf.Repeat(move_.z, range_ * 2.0f);
+	}
+
+}
---
+++ Assets/testShader/Shader/realSnowShader.shader
@@ -0,0 +1,83 @@
+﻿// Upgrade NOTE: replaced 'mul(UNITY_MATRIX_MVP,*)' with 'UnityObjectToClipPos(*)'
+
+Shader "Custom/realSnowShader" {
+	Properties {
+		_Color ("Color", Color) = (1,1,1,1)
+		_MainTex ("Base (RGB)", 2D) = "white" {}
+	}
+
+	// リアル寄り雪の描画 ハンズオン
+	SubShader {
+		Tags { "Queue"="Transparent" "IgnoreProjector"="True" "RenderType"="Transparent" }
+		LOD 200
+		Zwrite Off
+		Cull Off
+		// アルファブレンドをミックスする
+		Blend SrcAlpha OneMinusSrcAlpha
+
+		Pass {
+			CGPROGRAM
+			#pragma vertex vert
+			#pragma fragment frag
+			#pragma target 3.0
+
+			#include "UnityCG.cginc"
+
+			uniform sampler2D _MainTex;
+
+			struct appdata_custom {
+				float4 vertex : POSITION;
+				float2 texcoord : TEXCOORD0;
+			};
+
+			struct v2f {
+				float4 pos:SV_POSITION;
+				float2 uv:TEXCOORD0;
+			};
+
+			fixed4 _Color;
+			float4x4 _PrevInvMatrix;
+			float3 _TargetPosition;
+			float _Range;
+			float _RangeR;
+			float _Size;
+			float3 _MoveTotal;
+			float3 _CamUp;
+
+
+			v2f vert (appdata_custom v) {
+				float3 target = _TargetPosition;
+				float3 trip;
+				float3 mv = v.vertex.xyz;
+				mv += _MoveTotal;
+				trip = floor(((target - mv) * _RangeR + 1) * 0.5);
+				trip *= (_RangeR * 2);
+				mv += trip;
+
+				float3 diff = _CamUp * _Size;
+				float3 finalposition;
+				float3 tv0 = mv;
+
+				{
+					float3 eyeVector = ObjSpaceViewDir(float4(tv0, 0));
+					float3 sideVector = normalize(cross(eyeVector, diff));
+					tv0 += (v.texcoord.x - 0.5f) * sideVector * _Size;
+					tv0 += (v.texcoord.y - 0.5f) * diff;
+					finalposition = tv0;
+				}
+				v2f o;
+				o.pos = UnityObjectToClipPos(float4(finalposition, 1));
+				o.uv = MultiplyUV(UNITY_MATRIX_TEXTURE0, v.texcoord);
+				return o;
+			}
+
+			fixed4 frag(v2f i) : SV_Target
+			{
+				return tex2D(_MainTex, i.uv);
+			}
+			ENDCG
+		}
+		
+	}
+	FallBack "Diffuse"
+}
