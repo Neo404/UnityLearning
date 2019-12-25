ff4bedaf47b324db46a6927e5d64816080615173
Neo404 <tell-tell-doll.a.fine.weather@docomo.ne.jp>
2019-12-26 00:10:32 +0900

    add 人型アニメーションコード追加

---
+++ Assets/TestAnimation/animationScript.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class animationScript : MonoBehaviour
+{
+    private Transform Hip;
+    private Transform LeftLeg;
+    private Transform RightLeg;
+
+    void Start()
+    {
+        Hip = transform.Find("Armature").Find("Hips");
+        LeftLeg = Hip.Find("Left leg");
+        RightLeg = Hip.Find("Right leg");
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 RotateAxis = new Vector3(1, 0, 0);
+
+        float pendulum = (float)Math.Sin(Time.time * Math.PI);
+        float minusValue = 60 * pendulum;
+        LeftLeg.rotation = Quaternion.AngleAxis(-180.0f + minusValue, RotateAxis);
+        RightLeg.rotation = Quaternion.AngleAxis(180.0f - minusValue, RotateAxis);
+    }
+}
