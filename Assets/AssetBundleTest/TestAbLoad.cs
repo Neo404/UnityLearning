61e30701b96f30235b6f669a888f212d3b7f6268
Neo404 <tell-tell-doll.a.fine.weather@docomo.ne.jp>
2019-12-25 23:28:35 +0900

    add アセットバンドルの生成とロード成功

--- Assets/AssetBundleTest/TestAbLoad.cs
+++ Assets/AssetBundleTest/TestAbLoad.cs
@@ -4,9 +4,25 @@ using UnityEngine;
 
 public class TestAbLoad : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    IEnumerator Start()
     {
-        
+        var ab = AssetBundle.LoadFromFile("Build01/abprefab");
+        if (ab == null)
+        {
+            yield break;
+        }
+
+        var abMaterial = AssetBundle.LoadFromFile("Build01/abmaterial");
+
+        var prefab = ab.LoadAsset<GameObject>("redCube");
+        var material = abMaterial.LoadAsset<Material>("red");
+        GameObject go = Instantiate(prefab);
+        go.GetComponent<Renderer>().material = material;
+
+        yield return new WaitUntil(() => Input.GetMouseButton(0));
+
+        ab.Unload(true);
+        abMaterial.Unload(true);
+        Debug.Log("Unload完了");
     }
 }
